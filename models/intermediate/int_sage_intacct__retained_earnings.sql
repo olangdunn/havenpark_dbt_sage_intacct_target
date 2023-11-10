e762d56dfcea68432c6357790a870175782c6122
Matt Tkachev <108092650+matt-tkachev@users.noreply.github.com>
2023-11-10 12:14:16 -0600

    Update int_sage_intacct__retained_earnings.sql

--- models/intermediate/int_sage_intacct__retained_earnings.sql
+++ models/intermediate/int_sage_intacct__retained_earnings.sql
@@ -6,6 +6,8 @@ with general_ledger_by_period as (
 retained_earnings_prep as (
     select
         period_first_day,
+        location_id,
+        location_name,
         'dbt Package Generated' as account_no,
         'Adj. Net Income' as account_title,
         'balancesheet' as account_type,
@@ -17,12 +19,14 @@ retained_earnings_prep as (
         sum(period_net_amount) as period_net_amount
     from general_ledger_by_period
     where account_type = 'incomestatement'
-    group by period_first_day, book_id, entry_state, currency
+    group by period_first_day, location_id, location_name, book_id, entry_state, currency
 ),
 
 final as (
     select
         period_first_day,
+        location_id,
+        location_name,
         account_no,
         account_title,
         account_type,
@@ -31,8 +35,8 @@ final as (
         classification,
         currency,
         entry_state,
-        sum(period_net_amount) over (partition by book_id, entry_state, currency
-            order by period_first_day rows between unbounded preceding and current row
+        sum(period_net_amount) over (partition by location_id, location_name, book_id, entry_state, currency
+            order by period_first_day, location_id, location_name rows between unbounded preceding and current row
         ) as amount
     from retained_earnings_prep
 )
